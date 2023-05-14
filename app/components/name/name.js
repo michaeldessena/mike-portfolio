0014d81097cdeb0bb167b8d636c971f2926c1aba
michaeldessena <michaeldessena@gmail.com>
2023-05-14 17:59:07 +0200

    Small changes

---
+++ app/components/name/name.js
@@ -0,0 +1,14 @@
+'use client';
+// See: https://nextjs.org/docs/getting-started/react-essentials
+
+import React from 'react';
+
+export default class Name extends React.Component {
+  render() {
+    return (
+      <div className="nameDiv">
+        <p className="name">Michael Dessena</p>
+      </div>
+      );
+  }
+}
